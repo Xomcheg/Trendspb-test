ae4e4117a6e678d7b3902b8acf895da3c84322a2
Xromenkov <xromenkovevgenii@mail.ru>
2019-12-04 23:57:05 +0300

    Добавлены и стилизованы секции: about, price, slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
 $(function(){
 
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
 
+    });
+    document.getElementById("slick-slide-control00").innerHTML="01";
+    document.getElementById("slick-slide-control01").innerHTML="02";
+    document.getElementById("slick-slide-control02").innerHTML="03";
+    document.getElementById("slick-slide-control03").innerHTML="04";
+    document.getElementById("slick-slide-control04").innerHTML="05";
 
   
 });
