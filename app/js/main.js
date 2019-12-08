8416bad9533cb90b61bf9f341866c0b687d483a4
Xromenkov <xromenkovevgenii@mail.ru>
2019-12-08 22:38:03 +0300

    Добавлены и стилизованы секции: brands, question, request, map, contact, footer

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ $(function(){
         arrows: false,
         dots: true,
         fade: true,
-
+        initialSlide: 2,
     });
     document.getElementById("slick-slide-control00").innerHTML="01";
     document.getElementById("slick-slide-control01").innerHTML="02";
@@ -12,5 +12,9 @@ $(function(){
     document.getElementById("slick-slide-control03").innerHTML="04";
     document.getElementById("slick-slide-control04").innerHTML="05";
 
+    // $('.question__item').on('click', function(){
+    //     $('.question__item-box').toggleClass('active');
+    // });
+   
   
 });
